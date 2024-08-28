a46002f21e204a4f0fbd1d0d19b14511b7efc1b9
maraaacc <marijacutura6@gmail.com>
2024-08-28 13:30:25 +0200

    Frontend Task

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -2,17 +2,19 @@ import React from "react";
 
 const Details = ({ activeCard }) => {
   return (
-    <div className="flex  w-full rounded-md bg-gray-300 ">
-      <p className="flex-inline text-4xl font-medium text-gray-900 m-6">
-        Details
-      </p>
-      {activeCard.realName && (
-        <div className="pl-6 leading-relaxed text-xl text-black ">
-          <p>-{activeCard.realName}</p>
-          <p>-{activeCard.playerName}</p>
-          <p>-{activeCard.asset}</p>
-        </div>
-      )}
+    <div className=" w-full rounded-md bg-gray-300 ">
+      <div className="flex">
+        <p className=" text-4xl font-medium text-gray-900 m-6">Details</p>
+      </div>
+      <div className="flex">
+        {activeCard.realName && (
+          <div className="pl-6 leading-relaxed text-xl text-black sm:pb:5">
+            <p>-{activeCard.realName}</p>
+            <p>-{activeCard.playerName}</p>
+            <p>-{activeCard.asset}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
